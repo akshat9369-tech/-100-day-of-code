d85efb0ec65cc2983b78d748f1a95c27d6738faa
Akshat Mishra <akshat936942@gmail.com>
2025-08-25 00:27:08 +0530

    daily question

---
+++ question8.c
@@ -0,0 +1,13 @@
+//Q8 Write a program to find and display the sum of the first n natural numbers.
+#include<stdio.h>
+int main(){
+int num, de= 0;
+printf("enter your natural number");
+scanf("%d", &num);
+for(int ac=0; ac<= num; ac++){
+ de += ac;
+}
+printf("  final sum of your values is %d\n", de);
+return 0;
+}
+
