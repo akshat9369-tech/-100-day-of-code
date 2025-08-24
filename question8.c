031f22150cd9d405959adee92456e86622376719
Akshat Mishra <akshat936942@gmail.com>
2025-08-25 00:42:43 +0530

    Update question8.c

--- question8.c
+++ question8.c
@@ -4,7 +4,7 @@ int main(){
 int num, de= 0;
 printf("enter your natural number");
 scanf("%d", &num);
-for(int ac=0; ac<= num; ac++){
+for(int ac=1; ac<= num; ac++){
  de += ac;
 }
 printf("  final sum of your values is %d\n", de);
