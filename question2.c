e89a02c7bfd5d2169bbb4defe0a7c0c311c70b6a
Akshat Mishra <akshat936942@gmail.com>
2025-08-24 23:09:23 +0530

    daliy question

---
+++ question2.c
@@ -0,0 +1,18 @@
+//Q2 (User Inputs, Operations & Output)
+//Write a program to input two numbers and display their sum, difference, product, and quotient.
+
+#include<stdio.h>
+int main(){
+float a1,a2, sum, dif, pro, quo;
+printf("enter the two number");
+scanf("%f%f", &a1, &a2);
+sum =a1+a2;
+dif= a1-a2;
+pro= a1*a2;
+quo= a1/a2;
+printf("the sum of your digits is %f\n", sum);
+printf("the Diff of your digits is %f\n", dif);
+printf("the product of your digits is %f\n", pro);
+printf("the Quotient of your digits is %f\n", quo);
+return 0;
+}
