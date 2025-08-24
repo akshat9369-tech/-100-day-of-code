a85bbb756a648a9a9c4f7893e2125a87cf162b8d
Akshat Mishra <akshat936942@gmail.com>
2025-08-24 23:48:44 +0530

    Update question4.c

--- question4.c
+++ question4.c
@@ -1,5 +1,4 @@
-//Q4 (User Inputs, Operations & Output)
-//Write a program to calculate the area and circumference of a circle given its radius.
+//Q4 Write a program to calculate the area and circumference of a circle given its radius.
 
 #include<stdio.h>
 int main(){
