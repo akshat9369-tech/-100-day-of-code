ded68d5f28f29f6932b418a55586389d932a255f
Akshat Mishra <akshat936942@gmail.com>
2025-09-05 16:37:03 +0530

    Update q31.c

--- day16/q31.c
+++ day16/q31.c
@@ -7,10 +7,8 @@ int main() {
     printf("Enter a number: ");
     scanf("%d", &num);
 
-    // Find the highest bit position
     int mask = 1 << (sizeof(int) * 8 - 1); // Start from the leftmost bit
 
-    // Skip leading zeros
     int started = 0;
     for (i = 0; i < sizeof(int) * 8; i++) {
         if (num & mask) {
@@ -22,8 +20,7 @@ int main() {
         mask >>= 1;
     }
 
-    // If number is 0
-    if (!started) {
+        if (!started) {
         printf("0");
     }
 
