cede3951b9f3ccb2c2ebbc7406b289a74f1cb2fe
Akshat Mishra <akshat936942@gmail.com>
2025-09-07 20:55:29 +0530

    hiu

--- day14/q28.c
+++ day14/q28.c
@@ -1,22 +1,17 @@
-//Write a program to print the product of even numbers from 1 to n.
-#include <stdio.h>
-
-int main() {
-    int n, product = 1;
-    int hasEven = 0;
-
-    printf("Enter a number: ");
-    scanf("%d", &n);
-
-    for (int i = 2; i <= n; i += 2) {
-        product *= i;
-        hasEven = 1;
-    }
-
-    if (hasEven)
-        printf("Product of even numbers from 1 to %d is: %d\n", n, product);
-    else
-        printf("No even numbers in the given range.\n");
-
-    return 0;
+#include<stdio.h>
+int main(){
+int num, pro=1;
+printf("enter your number");
+scanf("%d", &num);
+if (num <= 0){
+printf("wrong input");
+return 0;
 }
+for(int i=1; i<=num; i++){
+if ( i %2 == 0){
+pro *= i;
+}
+}
+printf("product of all even number is %d\n", pro);
+}
+
