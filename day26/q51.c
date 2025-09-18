45f98b4ddbfc32eeb80bdf334fca2c8810e154bf
Akshat Mishra <akshat936942@gmail.com>
2025-09-18 22:09:44 +0530

    ques

--- day26/q51.c
+++ day26/q51.c
@@ -1,21 +1,19 @@
 //q.50Write a program to print the following pattern:
-//    5
 //   45
 //  345
 // 2345
 //12345
-
 #include<stdio.h>
 int main(){
-int a, b,c row =5;
-for(a = row; a> = 1; a--){
+int a, b, c, row =5;
+for(a = row; a>= 1; a--){
     for( c = a ; c <= 5; c ++ ){
-        for (b = row -1; b >= 1; b --)){
+        for (b = row -1; b >= 1; b --){
             printf(" ");            
         }
-        printf("%d", c)
+        printf("%d", c);
     }
-    printf("\n")
+    printf("\n");
 }
 return 0;
 }
