4b7e0609230a4516a6ec296efefbcec5f104282a
Akshat Mishra <akshat936942@gmail.com>
2025-11-09 22:36:53 +0530

    hikl

--- day54/q104.c
+++ day54/q104.c
@@ -18,3 +18,32 @@ Output 3:
 -1
 
 */
+#include <stdio.h>
+
+int findPivotInteger(int n) {
+    int totalSum = n * (n + 1) / 2;
+
+    for (int x = 1; x <= n; x++) {
+        int leftSum = x * (x + 1) / 2;
+        int rightSum = totalSum - (x - 1) * x / 2;
+        if (leftSum == rightSum) {
+            return x;
+        }
+    }
+
+    return -1;
+}
+
+int main() {
+    int n;
+
+    // Input
+    printf("Enter a positive integer n: ");
+    scanf("%d", &n);
+
+    // Output
+    int pivot = findPivotInteger(n);
+    printf("%d\n", pivot);
+
+    return 0;
+}
--- day60/q110.c
+++ day60/q110.c
@@ -13,3 +13,27 @@ Output 2:
 5 1 3 4 2
 
 */
+#include <stdio.h>
+
+void printMaxInSubarrays(int arr[], int n, int k) {
+    for (int i = 0; i <= n - k; i++) {
+        int max = arr[i];
+        for (int j = 1; j < k; j++) {
+            if (arr[i + j] > max) {
+                max = arr[i + j];
+            }
+        }
+        printf("%d ", max);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 1, 4, 5, 2, 3, 6};
+    int k = 3;
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printMaxInSubarrays(arr, n, k);
+
+    return 0;
+}
